993397070e0fc5b78b57c205bea470df430e6450
eddison-poon <namidae@gmail.com>
2026-07-25 02:41:19 +0800

    Add rerun_of_execution_id to execution repository

--- python/repositories/execution_repository.py
+++ python/repositories/execution_repository.py
@@ -398,6 +398,7 @@ class ExecutionRepository(
                 item.build_version,
                 item.external_reference,
                 item.remarks,
+                item.rerun_of_execution_id,
                 *item.defect_ids,
                 *item.evidence_ids,
             ]
